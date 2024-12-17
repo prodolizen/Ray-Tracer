7646cacc160d34645e608300759089abf7903bf9
Oli <124746592+prodolizen@users.noreply.github.com>
2024-12-17 20:10:04 +0000

    changed ray intersect

--- GCP_RayTracer/GCP_Raytracer_Framework/RayTracer.cpp
+++ GCP_RayTracer/GCP_Raytracer_Framework/RayTracer.cpp
@@ -1,68 +1,56 @@
 #include "RayTracer.h"
 
 
-//glm::vec3 RayTracer::TraceRay(Ray ray, std::vector<Sphere> spheres, glm::vec3 _cameraPos)
-//{
-//    //TODO: when multiple objects in scene we need to go through each ones RayInstersect() and see which is closest then only Shade() that one
-//    // remove vec3 store in new intersect
-//    //Intersect _intersection = sphere.RayIntersect(ray); //intersect position (intersect struct val)
-//
-//    //if (_intersection._happened)
-//    //{
-//    //    glm::vec3 colour = sphere.Shade(_intersection._position, lightPos, lightColour);
-//    //    return colour;
-//    //}
-//    //else
-//    //    return glm::vec3(0, 0, 1);
-//
-//    for (int i = 0; i < spheres.size(); i++) //cycle through all spheres in scene
-//    {
-//        Intersect _intersection = spheres[i].RayIntersect(ray);
-//
-//        if (_intersection._happened) //if there is an intersection colour the pixel, break from the loop and then return the colour
-//        {
-//            pixelColour = spheres[i].Shade(_intersection._position, lightPos, lightColour, _cameraPos);
-//            break;
-//        }
-//        else
-//            pixelColour = glm::vec3(0, 0, 1); //if no intersection colour pixel to background colour
-//    }
-//
-//    return pixelColour;
-//}
-
 glm::vec3 RayTracer::TraceRay(Ray ray, std::vector<Sphere> spheres, glm::vec3 _cameraPos)
 {
-    glm::vec3 pixelColour = glm::vec3(0, 0, 0); // Default color (background, no intersection)
-    float closestT = FLT_MAX; // Initialize with the maximum float value
-    int closestSphereIndex = -1; // To store the index of the closest sphere
-
-    for (int i = 0; i < spheres.size(); i++) // Cycle through all spheres in the scene
+    for (int i = 0; i < spheres.size(); i++) //cycle through all spheres in scene
     {
         Intersect _intersection = spheres[i].RayIntersect(ray);
 
-        if (_intersection._happened) // If an intersection happens
+        if (_intersection._happened) //if there is an intersection colour the pixel, break from the loop and then return the colour
         {
-            // Calculate the distance to the intersection point
-            float t = glm::length(_intersection._position - ray._origin);
-
-            // If this intersection is closer than the previously found ones, update the closest intersection
-            if (t < closestT) {
-                closestT = t;
-                closestSphereIndex = i;
-            }
+            pixelColour = spheres[i].Shade(_intersection._position, lightPos, lightColour, _cameraPos);
+            break;
         }
-    }
-
-    // If there was a closest sphere found, shade it
-    if (closestSphereIndex != -1) {
-        Intersect closestIntersection = spheres[closestSphereIndex].RayIntersect(ray);
-        pixelColour = spheres[closestSphereIndex].Shade(closestIntersection._position, lightPos, lightColour, _cameraPos);
+        else
+            pixelColour = glm::vec3(0, 0, 1); //if no intersection colour pixel to background colour
     }
 
     return pixelColour;
-
-
 }
+
+//glm::vec3 RayTracer::TraceRay(Ray ray, std::vector<Sphere> spheres, glm::vec3 _cameraPos)
+//{
+//    glm::vec3 pixelColour = glm::vec3(0, 0, 0); // Default color (background, no intersection)
+//    float closestT = FLT_MAX; // Initialize with the maximum float value
+//    int closestSphereIndex = -1; // To store the index of the closest sphere
+//
+//    for (int i = 0; i < spheres.size(); i++) // Cycle through all spheres in the scene
+//    {
+//        Intersect _intersection = spheres[i].RayIntersect(ray);
+//
+//        if (_intersection._happened) // If an intersection happens
+//        {
+//            // Calculate the distance to the intersection point
+//            float t = glm::length(_intersection._position - ray._origin);
+//
+//            // If this intersection is closer than the previously found ones, update the closest intersection
+//            if (t < closestT) {
+//                closestT = t;
+//                closestSphereIndex = i;
+//            }
+//        }
+//    }
+//
+//    // If there was a closest sphere found, shade it
+//    if (closestSphereIndex != -1) {
+//        Intersect closestIntersection = spheres[closestSphereIndex].RayIntersect(ray);
+//        pixelColour = spheres[closestSphereIndex].Shade(closestIntersection._position, lightPos, lightColour, _cameraPos);
+//    }
+//
+//    return pixelColour;
+//
+//
+//}
 
 
